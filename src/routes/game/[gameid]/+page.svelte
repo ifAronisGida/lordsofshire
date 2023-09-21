b33850d2849c907afe64af9d584b9deaa2933e64
a.gida <a.gida@mortoff.hu>
2023-09-21 11:11:24 +0200

    end game

--- src/routes/game/[gameid]/+page.svelte
+++ src/routes/game/[gameid]/+page.svelte
@@ -2,12 +2,31 @@
 	import type { PageData } from './$types';
 	import { db, type GameData } from '$lib/firebase';
 	import { docStore } from 'sveltefire';
+	import AuthCheck from '$lib/components/AuthCheck.svelte';
+	import { doc, updateDoc } from 'firebase/firestore';
 
 	export let data: PageData;
 
 	let game = docStore<GameData>(db, `games/${data.gameid}`);
+
+	async function endGame() {
+		await updateDoc(doc(db, 'games', data.gameid), { isLive: false });
+		for (const player of $game!.players) {
+			console.log(player);
+			await updateDoc(doc(db, 'users', player.uid), { gameID: '' });
+		}
+	}
 </script>
 
-<h1 class="text-center m-4">Játék {data.gameid}</h1>
-<p class="text-center m-4">Játékosok: {JSON.stringify($game?.players)}</p>
-<p class="text-center m-4">Játék indítva: {$game?.isLive}</p>
+<AuthCheck>
+	{#if $game?.isLive}
+		<button class="fixed bottom-0 right-0 btn variant-soft-warning m-4 p-4" on:click={endGame}
+			>VÉGE</button
+		>
+		<h1 class="text-center m-4">Játék {data.gameid}</h1>
+		<p class="text-center m-4">Játékosok: {JSON.stringify($game?.players)}</p>
+		<p class="text-center m-4">Játék indítva: {$game?.isLive}</p>
+	{:else}
+		<h1 class="text-center m-4">Játék vége!</h1>
+	{/if}
+</AuthCheck>
